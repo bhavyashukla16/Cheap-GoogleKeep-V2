8db58c62658fb53e750c97188769b825cf86f6ac
bhavyashukla16 <73889955+bhavyashukla16@users.noreply.github.com>
2021-06-16 23:23:41 +0530

    backend url

--- src/helpers/fetchClient.js
+++ src/helpers/fetchClient.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const fetchClient = () => {
     const instance = axios.create({
-        //baseURL: "https://keeper-app-backend.herokuapp.com/notes"
-        baseURL: "http://localhost:5000/notes"
+        baseURL: "https://keeper-app-backend.herokuapp.com/notes"
+        //baseURL: "http://localhost:5000/notes"
     })
     return instance
 }
