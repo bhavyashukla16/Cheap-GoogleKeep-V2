997d28cb86d184d632f8e60a1d3e3455d4f36080
bhavyashukla16 <73889955+bhavyashukla16@users.noreply.github.com>
2021-07-19 11:51:12 +0530

    added pin note functionality

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import AddIcon from "@material-ui/icons/Add";
 import Fab from "@material-ui/core/Fab";
 import Zoom from "@material-ui/core/Zoom";
 import EditIcon from '@material-ui/icons/Edit';
-import AttachFileIcon from '@material-ui/icons/AttachFile';
+//import AttachFileIcon from '@material-ui/icons/AttachFile';
 var Loader = require('react-loader');
 
 function App() {
   const [notes, setNotes] = useState([]);
   const [note, setNote] = useState({
     title: "",
-    content: ""
+    content: "",
+    pinned: false
   });
 
   const [isExpanded, setIsExpanded] = useState(false);
@@ -34,7 +35,7 @@ function App() {
     })
     .catch((err) => console.log(err) )
     .finally(() => setLoading(true))
-  },[])
+  },[setNotes, setId])
 
   const handleChange = (e) => (
     setNote({
@@ -59,7 +60,8 @@ function App() {
     setLoading(true), 
     setNote({
       title: "",
-      content: ""
+      content: "",
+      pinned: false
     }) )
   }
 
@@ -78,19 +80,22 @@ function App() {
     var noteToBeEdited = notes.filter(note => note._id===id)
     //console.log(noteToBeEdited)
     setIsExpanded(true)
+    console.log(id)
     setId(id)
+    console.log(Id);
     setNote({
       title: noteToBeEdited[0].title,
-      content: noteToBeEdited[0].content
+      content: noteToBeEdited[0].content,
+      pinned: noteToBeEdited[0].pinned
     })
     }
 
-  const submitEditedNote = () => {
+  const submitEditedNote = (editedNote) => {
     setLoading(false);
-    Axios.put(`/${Id}`, note, { params: Id})
+    Axios.put(`/${Id}`, editedNote, { params: Id})
     .then((response) => {
       const restOfNotes = notes.filter(note => note._id!==Id)
-      setNotes([...restOfNotes, note])
+      setNotes([...restOfNotes, editedNote])
       console.log(response.data.message)
     })
     .catch((err) => console.log(err))
@@ -99,10 +104,26 @@ function App() {
       setLoading(true)
       setNote({
         title: "",
-        content: ""
+        content: "",
+        pinned: false
       })
     })
   }
+
+const pinNote = (id) => {
+  setLoading(false);
+  var noteToBePinned = notes.filter(note => note._id===id);
+  console.log(id);
+  noteToBePinned[0].pinned = !noteToBePinned[0].pinned;
+  Axios.put(`/${id}`, noteToBePinned[0], {params: id})
+  .then((response) => {
+    const restOfNotes = notes.filter(note => note._id!==id)
+    setNotes([...restOfNotes, noteToBePinned[0]]);
+    console.log(response.data.message)
+  })
+  .catch((err) => console.log(err))
+  .finally(() => setLoading(true));
+}
 
   return (
     <div>
@@ -128,7 +149,7 @@ function App() {
         />
         <Zoom in={isExpanded}>
         { Id!==0 ? 
-          <Fab onClick={submitEditedNote}>
+          <Fab onClick={() => submitEditedNote(note)}>
             <EditIcon />
           </Fab>
           :
@@ -138,23 +159,44 @@ function App() {
         }
         </Zoom>
       </form>
-      <div className="note">
-        <h1>Hello there!</h1>
-        <p>No nasty typing here mate</p>
-        <p id="pin">
-          <AttachFileIcon />
-        </p>
-      </div>
-      {notes.map((noteItem) => (
-        <Note
+      <h2 className="pinned-heading">PINNED</h2>
+      <div className="pinned-notes">
+        <div className="note">
+          <div id="pin" style={{position: "relative", right: "20px", bottom: "2px"}}>
+            <i class="fas fa-thumbtack"></i>
+          </div>
+          <h1>Hello there!</h1>
+          <p>No nasty typing here mate</p>
+        </div>
+        {notes.filter(oneNote => oneNote.pinned===true).map(noteItem => (
+          <Note
             key={noteItem._id}
             id={noteItem._id}
             title={noteItem.title}
             content={noteItem.content}
             onDelete={deleteNote}
             onEdit={editNote}
+            onPin={pinNote}
+            pinned={noteItem.pinned}
           />
-      ))}
+        ))}
+      </div>
+      { notes.filter(oneNote => oneNote.pinned===false).length!==0 &&
+      <h2 className="pinned-heading">OTHERS</h2> 
+      }
+      <div className="unpinned-notes">
+        {notes.filter(oneNote => oneNote.pinned===false).map((noteItem) => (
+          <Note
+              key={noteItem._id}
+              id={noteItem._id}
+              title={noteItem.title}
+              content={noteItem.content}
+              onDelete={deleteNote}
+              onEdit={editNote}
+              onPin={pinNote}
+            />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,27 +1,96 @@
 import React from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from '@material-ui/icons/Edit';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Note(props) {
 
+  //const options = ['pin', 'edit', 'delete'];
+  
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const handleDelete = () => {
     props.onDelete(props.id)
+    setAnchorEl(null);
   }
 
   const handleEdit = () => {
     props.onEdit(props.id)
+    setAnchorEl(null);
+  }
+
+  const handlePin = () => {
+    props.onPin(props.id)
+    setAnchorEl(null);
   }
 
   return (
     <div className="note">
+    <div className="dots-menu">
+      <IconButton
+          aria-label="more"
+          aria-haspopup="true"
+          onClick={handleClick}
+      ><MoreVertIcon style={{color: "#989898"}}/>
+      </IconButton>
+        <Menu
+          //id="long-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          PaperProps={{
+            style: {
+              width: '12ch',
+            },
+          }}
+        >
+        { !props.pinned &&
+        <MenuItem onClick={handlePin}>
+          <i class="fas fa-thumbtack" style={{position:"relative", left: "3px", top: "3px", color: "#f5ba13"}}></i>
+          <span style={{marginLeft: "14px", color: "#707070"}}> pin </span>
+        </MenuItem>
+        }
+        <MenuItem onClick={handleEdit}>
+          <EditIcon style={{color: "#f5ba13"}}/>
+          <span style={{marginLeft: "6px", color: "#707070"}}>edit</span>
+        </MenuItem>
+        <MenuItem onClick={handleDelete}>
+          <DeleteIcon style={{color: "#f5ba13"}}/>
+          <span style={{marginLeft: "6px", color: "#707070"}}>delete</span>
+        </MenuItem>
+        </Menu>
+      </div>
+      { props.pinned &&
+      <div 
+      id="pin" 
+      onClick={handlePin}
+      dat-toggle="tooltip"
+      dat-placement="top"
+      title="Unpin note"
+      style={{
+        position: "relative", 
+        left: "10px", 
+        top: "1px",
+        cursor: "pointer"
+        }}>
+        <i class="fas fa-thumbtack"></i>
+      </div>
+      }
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <p id="button" onClick={handleDelete}>
-        <DeleteIcon />
-      </p>
-      <p id="button" onClick={handleEdit} >
-        <EditIcon />
-      </p>
     </div>
   );
 }
